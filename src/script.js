d1b14e10dbdfc3a25a136d2209c8d52ab3853d78
Fabricio <contact@fabricioguardia.com>
2022-06-27 12:02:03 -0700

    ghosts and shadows

--- src/script.js
+++ src/script.js
@@ -138,7 +138,7 @@ const graveMaterial = new THREE.MeshStandardMaterial({ color: '#b2b6b1' })
 for(let i = 0; i < 50; i++)
 {
     const angle = Math.random() * Math.PI * 2 // Random angle
-    const radius = 4 + Math.random() * 5      // Random radius
+    const radius = 3.5 + Math.random() * 5      // Random radius
     const x = Math.cos(angle) * radius        // Get the x position using cosinus
     const z = Math.sin(angle) * radius        // Get the z position using sinus
 
@@ -151,6 +151,8 @@ for(let i = 0; i < 50; i++)
     // // Rotation
     grave.rotation.z = (Math.random() - 0.5) * 0.4
     grave.rotation.y = (Math.random() - 0.5) * 0.4
+
+    grave.castShadow = true
 
     // Add to the graves container
     graves.add(grave)
@@ -196,6 +198,30 @@ house.add(doorLight)
 // Fog
 const fog = new THREE.Fog('#262837', 1, 15)
 scene.fog = fog
+
+// GHOSTS
+const ghost1 = new THREE.PointLight('#ff00ff', 2, 3)
+scene.add(ghost1)
+
+const ghost2 = new THREE.PointLight('#00ffff', 2, 3)
+scene.add(ghost2)
+
+const ghost3 = new THREE.PointLight('#ffff00', 2, 3)
+scene.add(ghost3)
+
+moonLight.castShadow = true
+doorLight.castShadow = true
+ghost1.castShadow = true
+ghost2.castShadow = true
+ghost3.castShadow = true
+
+walls.castShadow = true
+bush1.castShadow = true
+bush2.castShadow = true
+bush3.castShadow = true
+bush4.castShadow = true
+
+floor.receiveShadow = true
 
 /**
  * Sizes
@@ -243,6 +269,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setClearColor('#262837')
+renderer.shadowMap.enabled = true
 
 /**
  * Animate
@@ -252,6 +279,22 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Ghosts
+    const ghost1Angle = elapsedTime * 0.5
+    ghost1.position.x = Math.cos(ghost1Angle) * 4
+    ghost1.position.z = Math.sin(ghost1Angle) * 4
+    ghost1.position.y = Math.sin(elapsedTime * 3)
+
+    const ghost2Angle = - elapsedTime * 0.32
+    ghost2.position.x = Math.cos(ghost2Angle) * 5
+    ghost2.position.z = Math.sin(ghost2Angle) * 5
+    ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+
+    const ghost3Angle = - elapsedTime * 0.18
+    ghost3.position.x = Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32))
+    ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5))
+    ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
 
     // Update controls
     controls.update()
